ceb243e0b2134b935442c62e9d02458dd4e856aa
Gabriel Marques <gabriel.marquesbiel@hotmail.com>
2023-10-16 18:39:06 -0300

    Aula 4

--- src/main/java/br/com/marques/todolist/task/TaskController.java
+++ src/main/java/br/com/marques/todolist/task/TaskController.java
@@ -7,12 +7,16 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.config.Task;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.marques.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -41,10 +45,28 @@ public class TaskController {
         var task = this.taskRepository.save(taskModel);
         return ResponseEntity.status(HttpStatus.OK).body(task);
       }
+      //Get
       @GetMapping("/")//trazer tudo relacionado ao usuario 
       public List<TaskModel> list(HttpServletRequest request) {
         var idUser = request.getAttribute("idUser");
         var tasks = this.taskRepository.findByIdUser((UUID) idUser);
         return tasks;
+      }
+      //Update
+      //PathVariable serve para passar como endpoint o id do usuario que esta requisitando por exemplo:
+      //http://localhost:8080/tasks/615156151-casdasda-516561561
+      @PutMapping("/{id}")
+      public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id){
+         var task =  this.taskRepository.findById(id).orElse(null);
+         var idUser = request.getAttribute("idUser");
+         if (!task.getIdUser().equals(idUser)) {
+          return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário não tem permissão para alterar essa tarefa");
+        }
+
+        Utils.copyNonNullProperties(taskModel, task);
+        var taskUpdated = this.taskRepository.save(task);
+        return ResponseEntity.ok().body(taskUpdated);
+
+
       }
 }
